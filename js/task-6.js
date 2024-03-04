206421a1a9630cdb7180fb7f95910f7a30ea9718
WaldemarYW <124668448+WaldemarYW@users.noreply.github.com>
2024-03-04 22:16:56 +0200

    hw 7 done
    so hard

---
+++ js/task-6.js
@@ -0,0 +1,38 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = parseInt(input.value);
+
+  if (amount < 1 || amount > 100 || isNaN(amount)) {
+    return;
+  }
+
+  boxesContainer.innerHTML = '';
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.append(box);
+    size += 10;
+  }
+
+  input.value = '';
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
